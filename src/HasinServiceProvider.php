8a141bd41a31d985517b1495f459f61c37b3393a
603707288 <603707288@qq.com>
2021-02-25 11:42:06 +0800

    Compatible laravel5.8

--- src/HasinServiceProvider.php
+++ src/HasinServiceProvider.php
@@ -4,16 +4,29 @@ namespace BiiiiiigMonster\Hasin;
 
 use BiiiiiigMonster\Hasin\Database\Eloquent\BuilderMixin;
 use BiiiiiigMonster\Hasin\Database\Eloquent\RelationMixin;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
+use ReflectionClass;
+use ReflectionMethod;
 
 class HasinServiceProvider extends ServiceProvider
 {
     public function register()
     {
         // Eloquent\Builder mixin，provides hasin series implementation
-        Builder::mixin(new BuilderMixin());
+        /**
+         * Why not use 'Builder::mixin(new BuilderMixin())'?
+         * Compatible laravel5.8!
+         */
+        $mixin = new BuilderMixin();
+        $methods = (new ReflectionClass($mixin))->getMethods(
+            ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED
+        );
+        foreach ($methods as $method) {
+            $method->setAccessible(true);
+            Builder::macro($method->name, $method->invoke($mixin));
+        }
         // Eloquent\Relation mixin，support for the bottom layer of hasin
         Relation::mixin(new RelationMixin());
     }
