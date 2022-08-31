aa393042f4eaad977d0c24dddf99dea2ea813a84
陆云峰 <v-luyunfeng@xiaomi.com>
2022-08-31 12:29:10 +0800

    fix(pest): update

--- tests/Feature/HasInTest.php
+++ tests/Feature/HasInTest.php
@@ -1,33 +1,31 @@
 <?php
 
-namespace BiiiiiigMonster\Hasin\Tests\Feature;
-
 use BiiiiiigMonster\Hasin\Tests\Models\User;
 
-it('hasIn test', function () {
+test('hasIn same as has', function () {
     $has = User::has('posts')->get();
     $hasIn = User::hasIn('posts')->get();
 
     expect($has->pluck('id'))->toEqual($hasIn->pluck('id'));
 });
 
-it('hasIn(greater than 2) test', function () {
-    $has = User::has('posts', '>', 2)->get();
-    $hasIn = User::hasIn('posts', '>', 2)->get();
+test('hasIn(gte 2) same as has(gte 2)', function () {
+    $has = User::has('posts', '>=', 2)->get();
+    $hasIn = User::hasIn('posts', '>=', 2)->get();
 
     expect($has->pluck('id'))->toEqual($hasIn->pluck('id'));
 });
 
-it('nested hasIn test', function () {
+test('nested hasIn same as nested has', function () {
     $has = User::has('posts.comments')->get();
     $hasIn = User::hasIn('posts.comments')->get();
 
     expect($has->pluck('id'))->toEqual($hasIn->pluck('id'));
 });
 
-it('nested hasIn(greater than 2) test', function () {
-    $has = User::has('posts.comments', '>', 2)->get();
-    $hasIn = User::hasIn('posts.comments', '>', 2)->get();
+test('nested hasIn(gte 2) same as nested has(gte 2)', function () {
+    $has = User::has('posts.comments', '>=', 2)->get();
+    $hasIn = User::hasIn('posts.comments', '>=', 2)->get();
 
     expect($has->pluck('id'))->toEqual($hasIn->pluck('id'));
 });
