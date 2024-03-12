cc9c1a2dd0609af0f18ebb64c05c44a2a5502063
biiiiiigmonster <biiiiiigmonster@users.noreply.github.com>
2024-03-12 04:16:08 +0000

    Fix styling

--- src/Database/Eloquent/RelationMixin.php
+++ src/Database/Eloquent/RelationMixin.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
-use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\MorphOneOrMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
